3d219a7a2fbd7f7d317edb30f5269fa8b8ae847c
Howard Lince III <hlince@gmail.com>
2018-07-07 21:27:54 +0800

    syntax :|

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [! -f /config/.Syncovery/Syncovery.cfg]; then
+if [ ! -f /config/.Syncovery/Syncovery.cfg ]; then
     echo "Setting configuration setting for webserver"
     exec /syncovery/SyncoveryCL SET /WEBSERVER=0.0.0.0
 fi
