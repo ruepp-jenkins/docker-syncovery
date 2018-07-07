0d151f8fcafb50735c7504899782fcbd2a94bd67
Howard Lince III <hlince@gmail.com>
2018-07-07 21:24:59 +0800

    only confiugring when config file doesn't exist.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Setting configuration setting for webserver"
-exec /syncovery/SyncoveryCL SET /WEBSERVER=0.0.0.0
+if [! -f /config/.Syncovery/Syncovery.cfg]; then
+    echo "Setting configuration setting for webserver"
+    exec /syncovery/SyncoveryCL SET /WEBSERVER=0.0.0.0
+fi
 
 echo "Starting Server"
 exec /SyncoveryCL
