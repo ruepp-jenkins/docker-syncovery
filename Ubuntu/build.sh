b1183067b28afe70a6d347f966fe365176236f7b
Jean-Michel Mercier <jeanmichel.mercier@newsbridge.io>
2023-01-03 11:46:12 +0100

    add build script

---
+++ Ubuntu/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+wget https://www.syncovery.com/linver_x86_64-Web.tar.gz.txt 2>/dev/null
+wget https://www.syncovery.com/linver_guardian_x86_64.tar.gz.txt 2>/dev/null
+wget https://www.syncovery.com/linver_rs_x86_64.tar.gz.txt 2>/dev/null
+
+mapfile -t syncovery <linver_x86_64-Web.tar.gz.txt
+mapfile -t guardian  <linver_guardian_x86_64.tar.gz.txt
+mapfile -t remote    <linver_rs_x86_64.tar.gz.txt
+
+SYNCOVERY_DOWNLOADLINK=${syncovery[2]}
+VERSION=${syncovery[4]}
+GUARD_DOWNLOADLINK=${guardian[2]}
+REMOTE_DOWNLOADLINK=${remote[2]}
+
+docker build . -t docker_syncovery:${VERSION}-ubuntu \
+  --build-arg SYNCOVERY_DOWNLOADLINK=${SYNCOVERY_DOWNLOADLINK} \
+  --build-arg GUARD_DOWNLOADLINK=${GUARD_DOWNLOADLINK} \
+  --build-arg REMOTE_DOWNLOADLINK=${REMOTE_DOWNLOADLINK}
+
